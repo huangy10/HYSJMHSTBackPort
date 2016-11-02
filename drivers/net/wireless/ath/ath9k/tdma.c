72329b4541148770d6394c4903c3adfa27bbe708
Woody Huang <woody1573@163.com>
2016-11-02 14:23:04 +0800

    create tdma files

---
+++ drivers/net/wireless/ath/ath9k/tdma.c
@@ -0,0 +1,11 @@
+//
+// Created by 黄延 on 2016/11/2.
+//
+// Copy Right Reserved.
+//
+// We implement the TMDA mechanism here.
+//
+
+
+#include "tdma.h"
+
---
+++ drivers/net/wireless/ath/ath9k/tdma.h
@@ -0,0 +1,12 @@
+//
+// Created by 黄延 on 2016/11/2.
+//
+// Copy Right Reserved.
+//
+// We implement the TMDA mechanism here.
+//
+
+#ifndef BACKPORTS_TDMA_H
+#define BACKPORTS_TDMA_H
+
+#endif //BACKPORTS_TDMA_H
